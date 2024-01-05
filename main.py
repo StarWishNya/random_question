17e983d5f4005b4c2dbd5ec5365e5d88132fba8b
StarWishNya <StarWishNya@outlook.com>
2024-01-05 20:30:00 +0800

    新增了窗口

--- main.py
+++ main.py
@@ -1,12 +1,56 @@
 import Initialize
 import random
 import tkinter as tk
+import tkinter.filedialog
+import tkinter.font
 
 window=tk.Tk()
+defautl_font=tkinter.font.nametofont("TkDefaultFont")
+defautl_font.configure(family="Microsoft Yahei",size=12)
 window.title("Test")
-window.geometry("500x300")
+lenth=512
+width=640
+left=(window.winfo_screenwidth()-lenth)/2
+top=(window.winfo_screenheight()-width)/2
+window.geometry("%dx%d+%d+%d"%(lenth,width,left,top))
+window.resizable(False,False)
 
-def test(questionlist):
+show=tk.Label(window,text="Please input the filename:")
+#show.place(x=0,y=0)
+sign=tk.Label(window,text="None")
+#sign.place(x=450,y=0)
+def panduan(key,inputs):
+    if '正确' in key:
+        if '正确' in inputs:
+            return True
+        else:
+            return False
+    elif '错误' in key:
+        if '错误' in inputs:
+            return True
+        else:
+            return False
+    else:
+        if key==inputs:
+            return True
+        else:
+            return False
+
+def randomtest(questionlist):
+    question=random.choice(questionlist)
+    ques_show=tk.Label(window,text=question.stem)
+    ques_show.place(x=0,y=0)
+
+def main():
+    filename=input("Please input the filename:")
+    questionlist = Initialize.initialize(filename)
+    model=input("Please choose a model: 1.random 2.sequence\n")
+    if model=="1":
+        randomtest(questionlist)
+    elif model=="2":
+        test(questionlist)
+main()
+'''def test(questionlist):
     for question in questionlist:
         print(question.stem)
         print(question.options)
@@ -51,14 +95,4 @@ def randomtest(questionlist):
                 break
             else:
                 print("Wrong!")
-                u_answer=input()
-
-def main():
-    filename=input("Please input the filename:")
-    questionlist = Initialize.initialize(filename)
-    model=input("Please choose a model: 1.random 2.sequence\n")
-    if model=="1":
-        randomtest(questionlist)
-    elif model=="2":
-        test(questionlist)
-main()
+                u_answer=input()'''	
